8429ccbccee19b4f019af8623911d78101a7cbdf
Matt Layman <matthewlayman@gmail.com>
2016-06-05 17:59:22 -0400

    Add Joke model and admin.

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from jokes.models import Joke
+
+
+admin.site.register(Joke)
--- jokes/models.py
+++ jokes/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Joke(models.Model):
+    question = models.TextField()
+    punchline = models.TextField()
+    laughs = models.PositiveIntegerField(default=0)
