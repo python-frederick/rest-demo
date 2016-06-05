ba6dda4bee739b5647e32b25f2f46ff79b0a9a94
Matt Layman <matthewlayman@gmail.com>
2016-06-05 18:30:40 -0400

    Add the JokeViewSet and connect a router.

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from jokes.models import Joke
+from jokes.serializers import JokeSerializer
+
+
+class JokeViewSet(viewsets.ModelViewSet):
+    queryset = Joke.objects.all()
+    serializer_class = JokeSerializer
--- lolz/urls.py
+++ lolz/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+
+from jokes.views import JokeViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'jokes', JokeViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
 ]
